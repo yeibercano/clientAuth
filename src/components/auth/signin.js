f649db55f81ca186bbeb7e91f8f093e7a83dbbe6
Yeiber Cano <yeibercano@gmail.com>
2016-07-17 21:35:41 -0700

    add form

--- src/components/auth/signin.js
+++ src/components/auth/signin.js
@@ -2,16 +2,22 @@ import React, { Component } from 'react';
 import { reduxForm } from 'redux-form';
 
 class Signin extends Component {
+	handleFormSubmit({ email, password }) {
+		console.log(email, password)
+	}
+	
 	render() {
+	  const { handleSubmit, fields: {email, password}} = this.props;
+
 		return (
-			<form>
+			<form onSubmit={handleSubmit(this.handleFormSubmit.bind(this))}>
 				<fieldset className="form-group">
-					<label htmlFor="">Email</label>
-					<input type="text" className="form-control"/>
+					<label>Email</label>
+					<input type="text" {...email} className="form-control"/>
 				</fieldset>
 				<fieldset className="form-group">
-					<label htmlFor="">Password</label>
-					<input type="text" className="form-control"/>
+					<label>Password</label>
+					<input type="text" {...password} className="form-control"/>
 				</fieldset>
 				<button action='submit' className="btn btn-primary">Sign in</button>
 			</form>
