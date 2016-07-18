6ee38f8f2122b879817b6bab5a672dd903036dd6
Yeiber Cano <yeibercano@gmail.com>
2016-07-17 22:14:42 -0700

    add action creator with redux-thunk

---
+++ src/actions/index.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+
+const API_URL = 'http:localhost:3090';
+
+export function signinUser(email, password) {
+	return function(dispatch) {
+		axios.post(`${API_URL}/signin`, { email, password });
+
+	}
+	// submit email, pass to server
+
+	//if request is good
+	// update state
+	// save JWT
+	// redirect to route...to
+
+
+	// if request is bad
+	// show error
+}
