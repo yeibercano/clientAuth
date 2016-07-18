5dec55ff9e23ab1612f0e4725b7958d7fd3deffd
Yeiber Cano <yeibercano@gmail.com>
2016-07-17 23:28:35 -0700

    push user to feature after sucess

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,21 +1,24 @@
 import axios from 'axios';
+import { browserHistory } from 'react-router';
 
 const API_URL = 'http://localhost:3090';
 
 export function signinUser(email, password) {
 	return function(dispatch) {
-		axios.post(`${API_URL}/signin`, { email, password });
-
+		// submit email, pass to server
+		axios.post(`${API_URL}/signin`, { email, password })
+			.then(response => {
+					// if (respose.status === 200)
+					//if request is good
+					// update state
+					// save JWT
+					// redirect to route...to
+					browserHistory.push('/feature');
+			})
+			.catch(() => {
+				// if request is bad
+				// show error
+			});
 	}
-	// submit email, pass to server
-
-	//if request is good
-	// update state
-	// save JWT
-	// redirect to route...to
-
-
-	// if request is bad
-	// show error
 }
 
