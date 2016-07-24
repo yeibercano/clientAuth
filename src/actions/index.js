7adde5afedda1135c2d4d85184ffd74050915f8e
Yeiber Cano <yeibercano@gmail.com>
2016-07-24 15:15:53 -0700

    save token to localStorage

--- src/actions/index.js
+++ src/actions/index.js
@@ -14,6 +14,7 @@ export function signinUser(email, password) {
 					// update state
 					dispatch({ type: AUTH_USER });
 					// save JWT
+					localStorage.setItem('token', response.data.token)
 					// redirect to route...to
 					browserHistory.push('/feature');
 			})
