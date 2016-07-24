c4bb389fa6c1d00c226926a6db98dfe8080e774a
Yeiber Cano <yeibercano@gmail.com>
2016-07-24 15:50:47 -0700

    add error state and validate in form

--- src/components/auth/signin.js
+++ src/components/auth/signin.js
@@ -6,6 +6,16 @@ class Signin extends Component {
 	handleFormSubmit({ email, password }) {
 		this.props.signinUser(email, password );
 	}
+
+	renderAlert() {
+		if (this.props.errorMessage) {
+			return ( 
+				<div className="alert alert-danger">
+					<strong>Oops!</strong> {this.props.errorMessage}
+				</div>
+			);
+		}
+	}
 	
 	render() {
 	  const { handleSubmit, fields: {email, password}} = this.props;
@@ -18,15 +28,22 @@ class Signin extends Component {
 				</fieldset>
 				<fieldset className="form-group">
 					<label>Password</label>
-					<input type="text" {...password} className="form-control"/>
+					<input type="password" {...password} className="form-control"/>
 				</fieldset>
+				{this.renderAlert()}
 				<button action='submit' className="btn btn-primary">Sign in</button>
 			</form>
 		);
 	}
 }
 
+function mapStateToProps(state) {
+	return {  errorMessage: state.auth.error };
+}
+
 export default reduxForm({ 
 	form:'signin',
 	fields: ['email', 'password']
-}, null, actions)(Signin);
+}, mapStateToProps, actions)(Signin);
+
+
