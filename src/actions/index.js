cd51195f2684974427b54f267da7013c0a119897
Yeiber Cano <yeibercano@gmail.com>
2016-07-24 15:22:36 -0700

    create error helper and add to AUTH_USER

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import { browserHistory } from 'react-router';
-import { AUTH_USER } from './types';
+import { AUTH_USER, UNAUTH_USER, AUTH_ERROR } from './types';
 
 const API_URL = 'http://localhost:3090';
 
@@ -21,7 +21,14 @@ export function signinUser(email, password) {
 			.catch(() => {
 				// if request is bad
 				// show error
+				dispatch(authError('Bad login info'));
 			});
 	}
 }
 
+export function authError (error) {
+	return {
+		type: AUTH_ERROR,
+		payload: error
+	}
+}
