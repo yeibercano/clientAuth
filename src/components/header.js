99fc273c0c60d6fe19157e7ee23011735395c1a3
Yeiber Cano <yeibercano@gmail.com>
2016-07-24 18:30:59 -0700

    show signin or signout depending on authUser

--- src/components/header.js
+++ src/components/header.js
@@ -1,17 +1,42 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router';
 
 class Header extends Component {
+	renderLinks() {
+		if (this.props.authenticated) {
+	   	return	<li className="nav-item">
+				<Link className="nav-link" to='/signout'>Sign Out</Link>
+			</li>
+		} else {
+			return [
+				<li className="nav-item" key={1}>
+					<Link className="nav-link" to="./signin">Sign in</Link>
+				</li>,
+				<li className="nav-item" key={2}>
+					<Link className="nav-link" to='/signup'>Sign up</Link>
+				</li>
+			];
+		}
+	}
+ 
 	render() {
+		console.log('this/props', this.props)
 		return (
 			<nav className="navbar navbar-light">
+				<Link to='/' className="navbar-brand">Home</Link>
 				<ul className="nav navbar-nav">
-					<li className="nav-item">
-						Sing in
-					</li>
+					{this.renderLinks()}
 				</ul>
 			</nav>
 		)
 	}
 }
 
-export default Header;
+function mapStateToProps(state) {
+	return {
+		authenticated: state.auth.authenticated
+	}
+}
+
+export default connect(mapStateToProps)(Header);
