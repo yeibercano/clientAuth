12ca4623bfda8e19905cfb1aba2df38e7f5421b0
Yeiber Cano <yeibercano@gmail.com>
2016-07-24 18:40:09 -0700

    fix typo extra '.'

--- src/components/header.js
+++ src/components/header.js
@@ -11,7 +11,7 @@ class Header extends Component {
 		} else {
 			return [
 				<li className="nav-item" key={1}>
-					<Link className="nav-link" to="./signin">Sign in</Link>
+					<Link className="nav-link" to="/signin">Sign in</Link>
 				</li>,
 				<li className="nav-item" key={2}>
 					<Link className="nav-link" to='/signup'>Sign up</Link>
