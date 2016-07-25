9d1a387fdbd7eadd7202ce516127bad3555f0406
Yeiber Cano <yeibercano@gmail.com>
2016-07-24 19:16:59 -0700

    create basic form

--- src/components/auth/signup.js
+++ src/components/auth/signup.js
@@ -1,13 +1,34 @@
 import React, { Component } from 'react';
-import {reduxForm } from 'redux-form';
+import { reduxForm } from 'redux-form';
 import * as actions from '../../actions';
 
 class Signup extends Component {
 	render() {
+	 const { handleSubmit, fields: { email, password, passwordConfirm }} = this.props;
+		
 		return (
-			<div>Signup</div>
+			<form>
+				<fieldset className="form-group">
+					<label htmlFor="">Email:</label>
+					<input className="form-control" {...email} />
+				</fieldset>
+				<fieldset className="form-group">
+					<label htmlFor="">Password:</label>
+					<input className="form-control glyphicon" {...password} type='password' />
+				</fieldset>
+				<fieldset className="form-group">
+					<label >Confirm Password:</label>
+					<input className="form-control" {...passwordConfirm} type='password' />
+				</fieldset>
+				<button action='submit' className="btn btn-primary"> Sign up!</button>
+			</form>
 		)
 	}
 }
 
-export default Signup;
+
+
+export default reduxForm({
+	form: 'signup',
+	fields: ['email', 'password', 'passwordConfirm']
+})(Signup);
